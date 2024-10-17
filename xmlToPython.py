220bff2e2c87ebb26c9b57ec6e189f6eb11c0936
Raúl Juárez <raul.juarez.7e8@itb.cat>
2024-10-17 17:06:18 +0200

    commitRaúl

--- xmlToPython.py
+++ xmlToPython.py
@@ -10,36 +10,30 @@ try:
     root = ET.fromstring(contenido)
 
     # Mostrar información del usuario
-    print("Información del Usuario:")
-    for usuario in root.iter('usuario'):
-        mail = usuario.find('mail').text.strip().replace('"', '') if usuario.find(
-            'mail') is not None else 'No disponible'
-        protDatos = usuario.find('protDatos').text.strip().replace('"', '') if usuario.find(
-            'protDatos') is not None else 'No disponible'
-        nombre = usuario.find('nombre').text.strip().replace('"', '') if usuario.find(
-            'nombre') is not None else 'No disponible'
+    for incidencia in root.iter('incidencia'):
+        usuario = incidencia.find('usuario')
+        mail = usuario.find('mail').text.strip()
+        protDatos = usuario.find('protDatos').text.strip()
+        nombre = usuario.find('nombre').text.strip()
 
     # Mostrar información del problema
-    print("\nInformación del Problema:")
-    for problema in root.iter('problema'):
-        aula = problema.find('aula').text.strip().replace('"', '')
-        fecha = problema.find('fecha').text.strip().replace('"', '') if problema.find(
-            'fecha') is not None else 'No disponible'
-        id_ = problema.find('id').text.strip().replace('"', '') if problema.find('id') is not None else 'No disponible'
-        tipoP = problema.find('tipoP').text.strip().replace('"', '') if problema.find(
-            'tipoP') is not None else 'No disponible'
-        descripcion = problema.find('descripción').text.strip().replace('"', '') if problema.find(
-            'descripción') is not None else 'No disponible'
-        urgencia = problema.find('urgencia').text.strip().replace('"', '')
-        propuesta = problema.find('propuesta').text.strip().replace('"', '') if problema.find('propuesta') is not None else 'No disponible'
-
+        problema = incidencia.find('problema')
+        aula = problema.find('aula').text.strip()
+        fecha = problema.find('fecha').text.strip()
+        id_ = problema.find('id').text.strip()
+        tipoP = problema.find('tipoP').text.strip()
+        descripcion = problema.find('descripción').text.strip()
+        urgencia = problema.find('urgencia').text.strip()
+        #Para la etiqueta propuesta, pedimos que si no encuentra contenido en la palabra propuesta, se indique "No se especifica"
+        propuesta = problema.find('propuesta').text.strip() if (problema.find('propuesta') is not None and
+        problema.find('propuesta').text is not None) else 'No se especifica.'
 
 #Imprimos toda la informacion
-        print("Usuario:")
+        print("\nInformación del Usuario:")
         print(f"- Nombre: {nombre}")
         print(f"  Mail: {mail}")
         print(f"  Protección de Datos: {protDatos}")
-        print("Problema:")
+        print("\nInformación del Problema:")
         print(f"- ID: {id_}")
         print(f"  Aula: {aula}")
         print(f"  Fecha: {fecha}")
@@ -47,6 +41,6 @@ try:
         print(f"  Descripción: {descripcion}")
         print(f"  Urgencia: {urgencia}")
         print(f"  Propuesta: {propuesta}")
-        print(" ")
+
 except ET.ParseError as e:
     print(f"Error al analizar el XML: {e}")
