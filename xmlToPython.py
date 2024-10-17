85266bcb26b1a12b613a38aa44fa826300d0be61
Raúl Juárez <raul.juarez.7e8@itb.cat>
2024-10-17 18:33:47 +0200

    commitRaúl

--- xmlToPython.py
+++ xmlToPython.py
@@ -1,13 +1,12 @@
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as eT
 
 # Leer el archivo y reemplazar entidades problemáticas
 with open('formularioIncidenciasGrupo2.xml', 'r', encoding='utf-8') as f:
     contenido = f.read()
-    contenido = contenido.replace('&', '&amp;')  # Escapar &
 
-# Parsear el contenido modificado
+# Parsear el siguiente código
 try:
-    root = ET.fromstring(contenido)
+    root = eT.fromstring(contenido)
 
     # Mostrar información del usuario por cada incidencia que haya en el xml
     for incidencia in root.iter('incidencia'):
@@ -25,15 +24,14 @@ try:
         descripcion = problema.find('descripción').text.strip()
         urgencia = problema.find('urgencia').text.strip()
         #Para la etiqueta propuesta, pedimos que si no encuentra contenido en la palabra propuesta, se indique "No se especifica"
-        propuesta = problema.find('propuesta').text.strip() if (problema.find('propuesta') is not None and
-        problema.find('propuesta').text is not None) else 'No se especifica.'
+        propuesta = problema.find('propuesta').text.strip() if problema.find('propuesta').text is not None else 'No se especifica.'
 
-#Imprimimos toda la informacion usando \n para añadir interlineados y poder leerlo mejor
-        print("\n\n- Información del Usuario:")
+#Imprimimos toda la información
+        print("\n\033[1m- Información del Usuario.\033[0m")
         print(f"  Nombre: {nombre}")
         print(f"  Mail: {mail}")
         print(f"  Protección de Datos: {protDatos}")
-        print("\n- Información del Problema:")
+        print("\033[1m- Información del Problema.\033[0m")
         print(f"  ID: {id_}")
         print(f"  Aula: {aula}")
         print(f"  Fecha: {fecha}")
@@ -42,5 +40,5 @@ try:
         print(f"  Urgencia: {urgencia}")
         print(f"  Propuesta: {propuesta}")
 
-except ET.ParseError as e:
+except eT.ParseError as e:
     print(f"Error al analizar el XML: {e}")
