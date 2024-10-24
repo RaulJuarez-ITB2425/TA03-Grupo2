0e41a024460a3a6d25eb1fa2becac9618c8a1087
RaulJuarez-ITB2425 <raul.juarez.7e8@itb.cat>
2024-10-24 09:29:22 +0200

    prueba con filtros usando import datetime

--- xmlToPython.py
+++ xmlToPython.py
@@ -1,16 +1,24 @@
 import xml.etree.ElementTree as eT
 import json
+import datetime
 
 # Leer el archivo y reemplazar entidades problemáticas
 with open('formularioIncidenciasGrupo2.xml') as f:
     contenidoForm = f.read()
 
-# Lista para almacenar las incidencias
+# Lista para almacenar las incidencias en un array
 lista_incidencias = []
 
 # Parsear el siguiente código
 try:
     root = eT.fromstring(contenidoForm)
+
+
+    def convertir_fecha(fecha_usu):
+        return datetime.strptime(fecha_usu, '%d/%m/%Y')
+
+    fecha_inicio_usu = input("Ingresa la fecha de inicio (DD-MM-YYYY): ")
+    fecha_fin_usu = input("Ingresa la fecha de fin (DD-MM-YYYY): ")
 
     # Mostrar información del usuario por cada incidencia que haya en el xml
     for incidencia in root.iter('incidencia'):
@@ -22,13 +30,16 @@ try:
     # Mostrar información del problema por cada incidencia que haya en el xml
         problema = incidencia.find('problema')
         aula = problema.find('aula').text.strip()
-        fecha = problema.find('fecha').text.strip()
+        fecha_usu = problema.find('fecha').text.strip()
+        fecha = convertir_fecha(fecha_usu)
         id_ = problema.find('id').text.strip()
         tipoP = problema.find('tipoP').text.strip()
         descripcion = problema.find('descripción').text.strip()
         urgencia = problema.find('urgencia').text.strip()
         #Para la etiqueta propuesta, pedimos que si no encuentra contenido en la palabra propuesta, se indique "No se especifica"
         propuesta = problema.find('propuesta').text.strip() if problema.find('propuesta').text is not None else 'No se especifica.'
+
+
 
 #Imprimimos toda la información
         print("\n\033[1m- Información del Usuario.\033[0m")
